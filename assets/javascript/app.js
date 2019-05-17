2e2b1fc36eb2e20ae193772b66a6268d39aae33c
Geronimo Perez <Geronimo.Perez4@gmail.com>
2019-05-16 23:44:25 -0400

    modified style.css, app.js and index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,6 @@
 
-let i = 0;
-let i2 = -1;
-let i3 = 0;
-let randomY;
-let randomX;
-let Ans = [];
-let rightAns;
-let handLeft;
-let handRight;
-let intervalId;
-let correctAns;
-let time = 16;
-let rightAnsTotal = 0;
-let wrongAnsTotal = 0;
 // Sounds
+// ======================================================================================
 const themeSong = document.createElement('audio');
 $(themeSong).attr('src', "assets/sounds/Halo Theme Song Original.mp3");
 const rightAnsSong = document.createElement('audio');
@@ -32,79 +19,112 @@ const lostLaugh = document.createElement('audio');
 $(lostLaugh).attr('src', 'assets/sounds/man-laughing.mp3')
 const lostDoor = document.createElement('audio');
 $(lostDoor).attr('src', 'assets/sounds/creaky-wood-door.mp3')
-//random background
-let ground = ['./assets/images/background0.png', './assets/images/background1.png', './assets/images/background2.png', './assets/images/background3.png', './assets/images/background4.png', './assets/images/background5.png', './assets/images/background6.png', './assets/images/background7.png'];
-// hands
-handRight = ['assets/images/rightHand0.png', 'assets/images/rightHand1.png', 'assets/images/rightHand2.png', 'assets/images/rightHand3.png'];
-handLeft = ['assets/images/leftHand0.png', 'assets/images/leftHand1.png', 'assets/images/leftHand2.png', 'assets/images/leftHand3.png'];
-// Questions and answers
-const qAndA = [
-	["What is the spirit?", "a ghost", "what we become once we die", "what God and the Devil are fighting for", "the nonphysical part of a person"],
-	["What is God if it's not defined by religion?", "Jesus", "nothing", "Allah", "the universe"],
-	["If iternal life begins with an eternal sleep, when will the spirit wake up?", "right away but it cannot be seen because is in a nonphysical form", "right away somewhere else", "after an eternity", "never"],
-	["Can the flow of enery teach us about right and wrong?", "no, because enery cannot reason", "yes, because enery always follow the shortest path", "no, because enery is not alive", "yes, because it can inspire purpose by it's persuit of balance"],
-	["What is the meaning of evil?", "the absence of good", "all the Devil does", "ignorance", "depravity"],
-	["What is the meaning of good?", "light", "what is needed to go to heaven", "the absence of evil", "righteousness"],
-	["According to popular views, what is the meaning of life?", "to discover the creator", "to focus on spiritual growth and serive to humanity", "to seek devine salvation through the grace of God", "to resolve the imbalance of the mind by understanding the nature of reality"]
+const ground = ['./assets/images/background0.png', './assets/images/background1.png', './assets/images/background2.png', './assets/images/background3.png', './assets/images/background4.png', './assets/images/background5.png', './assets/images/background6.png', './assets/images/background7.png'];
+const handRight = ['assets/images/rightHand0.png', 'assets/images/rightHand1.png', 'assets/images/rightHand2.png', 'assets/images/rightHand3.png'];
+const handLeft = ['assets/images/leftHand0.png', 'assets/images/leftHand1.png', 'assets/images/leftHand2.png', 'assets/images/leftHand3.png'];
+// ==============================================================================================
+let questionIndex = 0;
+let intervalId;
+let correctAns;
+let time = 16;
+let rightAnsTotal = 0;
+let wrongAnsTotal = 0;
+// Questions and Answers
+// ==============================================================================================
+let qAndA = [
+	{
+		question: "What is the spirit?",
+		answers: ["a ghost", "what we become once we die", "what God and the Devil are fighting for", "the nonphysical part of a person"]
+	},
+	{
+		question: "What is God if it's not defined by religion?",
+		answers: ["Jesus", "nothing", "Allah", "the universe"]
+	},
+	{
+		question: "If iternal life begins with an eternal sleep, when will the spirit wake up?",
+		answers: ["right away but it cannot be seen because is in a nonphysical form", "right away somewhere else", "after an eternity", "never"]
+	},
+	{
+		question: "Can the flow of enery teach us about right and wrong?",
+		answers: ["no, because enery cannot reason", "yes, because enery always follow the shortest path", "no, because enery is not alive", "yes, because it can inspire purpose by it's persuit of balance"]
+	},
+	{
+		question: "What is the meaning of evil?",
+		answers: ["the absence of good", "all the Devil does", "ignorance", "depravity"]
+	},
+	{
+		question: "What is the meaning of good?",
+		answers: ["light", "what is needed to go to heaven", "the absence of evil", "righteousness"]
+	},
+	{
+		question: "According to popular views, what is the meaning of life?",
+		answers: ["to discover the creator", "to focus on spiritual growth and serive to humanity", "to seek devine salvation through the grace of God", "to resolve the imbalance of the mind by understanding the nature of reality"]
+	}
 ];
+// ============================================================================================
+
+// Start App
+// ============================================================================================
 // Random question
-let randomQ = Math.floor(Math.random() * qAndA.length);
-
-// =============================================================================
-
+qAndA = qAndA.sort(() => 0.5 - Math.random());
 // Random background
 $('body').css("background-image", `url('${ground[Math.floor(Math.random() * ground.length)]}')`);
-
+// move hands
+const moveHands = setInterval(move, 4000);
 // Start Triva Game
 $('#startButton').on("click", startGame);
+//check answer
+$('.ans').on('click', check);
+// ============================================================================================
 
-// =============================================================================
-
+// Functions definition
+// ============================================================================================
 function startGame() {
 	themeSong.play();
 	$("#startDiv").hide();
-	$("#bodyRows").show(1000);
+	$("#bodyRows").show(5000);
 	displayQuestions();
 };
 
-//hide scoreBoard
-$('#scoreBoard').css("opacity", '0');
+function displayQuestions() {
+	timer();
+	//display randomized question
+	$('#Question').html(qAndA[questionIndex].question);
+	//correct answer
+	correctAns = qAndA[questionIndex].answers[3];
+	//display randomized answers 
+	let randomAnswers = qAndA[questionIndex].answers.sort(() => 0.5 - Math.random());
+	randomAnswers.forEach((answer, i) => {
+		$('#ans' + (i + 1)).html(answer);
+	});
+};
 
-function scoreboard() {
-	if (i >= qAndA.length) {
+function nextQuestion() {
+	if (questionIndex >= qAndA.length) {
 		clearInterval(intervalId);
 		$('#bodyRows').hide();
+		themeSong.pause();
 		score();
 	}
-
 	else {
 		displayQuestions();
 	};
 };
+
 function score() {
-	$('#scoreBoard').animate({ opacity: '1' });
+	$('#scoreBoard').show(1000);
 	$('#rightAns').html(rightAnsTotal);
 	$('#wrongAns').html(wrongAnsTotal);
 
 	if (rightAnsTotal > wrongAnsTotal) {
-		themeSong.play();
 		winSong.play();
 		winTrump.play();
 		winBells.play();
-
 		$('#title1').css('color', 'white');
 		$('#rightHand').attr('src', handRight[3]);
 		$('#rightHand').animate({ height: '+=5%', width: '+=5%' }, 3000);
-		for (var i = 0; i < 5; i++) {
-			$('#rightHand').animate({ top: '+=5%' }, 1000);
-			$('#rightHand').animate({ top: '-=5%' }, 1000);
-		}
 		$('#leftHand').attr('src', handLeft[3]);
 		$('#leftHand').animate({ height: '+=10%', width: '+=10%', right: '-=5%' }, 3000);
-		for (var i = 0; i < 5; i++) {
-			$('#leftHand').animate({ height: '+=10%', width: '+=10%', right: '-=5%' }, 3000);
-			$('#leftHand').animate({ height: '-=10%', width: '-=10%', right: '+=5%' }, 3000);
-		}
 	}
 	else {
 		themeSong.play();
@@ -131,48 +151,12 @@ function move2() {
 	});
 };
 
-function displayQuestions() {
-
-	timer();
-	move();
-
-	//display randomized question
-	randomQ++;
-	if (randomQ >= qAndA.length) {
-		randomQ = 0;
-	}
-	$('#Question').html(qAndA[randomQ][0]);
-
-	//correct answer
-	correctAns = qAndA[randomQ][4];
-
-	//randomize the answers in array
-	i2 = Math.floor(Math.random() * 4 + 1);
-	i3 = 0;
-	for (let y = i2; y < 5; y++) {
-		Ans[i3] = (qAndA[randomQ][y]);
-		i3++
-	};
-	for (let y = 1; y < i2; y++) {
-		Ans[i3] = (qAndA[randomQ][y]);
-		i3++
-	};
-	//display randomized answers
-	for (let y = 1; y < 5; y++) {
-		$('#ans' + y).html(Ans[y - 1])
-	};
-};
-
-function nextQuestion() {
-	i++;
-	move();
-	scoreboard();
-}
 function timer() {
 	time = 16;
 	clearInterval(intervalId);
 	intervalId = setInterval(decrement, 1000);
 };
+
 function decrement() {
 	time--;
 	$("#timer").html(time);
@@ -183,30 +167,25 @@ function decrement() {
 		nextQuestion();
 	};
 };
-//hands random position
-function randomPosition() {
-	randomY = Math.floor(Math.random() * 8 + 26);
-	randomX = Math.floor(Math.random() * 4 + 8);
-}
-function move() {
-	randomPosition();
-	$('#rightHand').animate({ top: randomY + '%', left: randomX + '%' }, 3000);
-	$('#leftHand').animate({ top: randomY + '%' }, 3000);
-};
-//check answer
-$('.ans').on('click', check);
 
-function check(questionIndex) {
-	console.log(Ans);
-	console.log(event.target.innerHTML)
+//hands random position
+function move() {
+	$('#rightHand').animate({ top: `${Math.random()*8+26}%`, left: `${Math.random()*4+8}%` }, 3000);
+	$('#leftHand').animate({ top: `${Math.random()*8+26}%`, right: `${Math.random()*4+8}%` }, 3000);
+};
+
+function check() {
 	const userAns = event.target.innerHTML;
+	console.log(qAndA[questionIndex].question)
+	console.log("User Answer: ", userAns);
+	console.log("Correct Answer: ", qAndA[questionIndex].answers[3]);
 	if (userAns === correctAns) {
 		rightAnsTotal++
 		rightAnsSong.play();
-		nextQuestion();
 	} else {
 		wrongAnsTotal++;
 		wrongAnsSong.play();
-		nextQuestion();
-	}
+	};
+	questionIndex++;
+	nextQuestion();
 }
