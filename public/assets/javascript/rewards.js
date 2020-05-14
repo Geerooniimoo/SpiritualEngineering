5005b515d588957d8fb9caffc292f0e6797aa138
Geronimo Perez <geronimo.perez4@gmail.com>
2020-05-14 00:16:58 -0400

    Corrected typo

--- public/assets/javascript/rewards.js
+++ public/assets/javascript/rewards.js
@@ -1,5 +1,5 @@
 var rewards = [
-    '<h4 class="reward">Emotions are a power source not an outcome.  Fear should only be the begining of something courages.</h4>',
+    '<h4 class="reward">Emotions are a power source not an outcome.  Fear should only be the beginning of something courageous.</h4>',
     '<h4 class="reward">Everything can seem like anything if you look at it from everywhere at a specific point in time.</h4>  <h4 class="reward">If you cannot bare what you cannot change, change your point of view and continue forward.</h4>',
     '<h4>Life is a state of change in a period of time.</h4><h4>It is only worth having if change is for the better.</h4>',
     '<h4>Good does no evil.</h4><h4>Don\'t let the means justify the ends confuse you.</h4>'
